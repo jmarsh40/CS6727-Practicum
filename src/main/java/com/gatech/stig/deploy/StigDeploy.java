352853254ed18e6f868664d9864a9b1e735a17a3
root <root@vbox>
2026-05-30 15:43:30 -0600

    Initial commit

---
+++ src/main/java/com/gatech/stig/deploy/StigDeploy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gatech.stig.deploy;
+import java.io.File;
+
+/**
+ *
+ * @author root
+ */
+public class StigDeploy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
