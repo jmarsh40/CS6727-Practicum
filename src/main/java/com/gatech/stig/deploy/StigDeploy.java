c3d7813efd580ce51fcfe2646bc6f214939de893
Ragnarak <Ragnarak@DESKTOP-G2J2H9B>
2026-06-06 16:35:57 -0600

    Updating main menu

--- src/main/java/com/gatech/stig/deploy/StigDeploy.java
+++ src/main/java/com/gatech/stig/deploy/StigDeploy.java
@@ -4,14 +4,22 @@
 
 package com.gatech.stig.deploy;
 import java.io.File;
+import java.util.Scanner;
 
 /**
  *
  * @author root
  */
 public class StigDeploy {
-
+    private static final String menu = "Please select an option: \nCreate a new configuration: 1 \nLoad a configuration: 2 \nExit: 3\n";
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner selector = new Scanner(System.in);
+        while (true) {
+            System.out.println(menu);
+            String choice = selector.nextLine();
+            if ("3".equals(choice)) {
+                return;
+            }
+        }
     }
 }
